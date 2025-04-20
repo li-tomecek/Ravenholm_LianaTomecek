a25e199b5a64bd9f54a13530e5ecdbfead2be39d
Liana Tomecek <liana.tomecek@gmail.com>
2025-04-20 12:06:56 -0600

    enemy firing bullets with inaccuracy at player

---
+++ Assets/Scripts/Room 1 & 2/Bullet.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] protected float _timeToDespawn = 5f;
+    protected float _timeAlive;
+    void Update()
+    {
+        _timeAlive += Time.deltaTime;
+        if (_timeAlive >= _timeToDespawn)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Room 1 & 2/Turret.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [Header("Gun")]
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _bulletOrigin;
+    
+    [SerializeField] private float _fireRate = 1f;
+    [SerializeField] private float _fireVelocity = 10f;
+
+    
+    [Header("Accuracy and Spread")]
+    [SerializeField] private float _maxVerticalSpread;
+    [SerializeField] private float _maxHorizontalSpread;
+
+    private bool _firing = true;
+
+    void Start()
+    {
+        StartCoroutine(FiringCycle());
+    }
+
+    private IEnumerator FiringCycle()
+    {
+        Quaternion rotation;
+       
+        while (_firing)
+        {
+            //calculate random offset of next bullet
+            rotation = Quaternion.Euler(
+                Random.Range(-_maxVerticalSpread, _maxVerticalSpread), 
+                Random.Range(-_maxHorizontalSpread, _maxHorizontalSpread), 
+                0f);
+
+
+            FireBullet((rotation * (_target.position - _bulletOrigin.position)).normalized);
+
+            yield return new WaitForSeconds(_fireRate);
+        }
+    }
+    
+    private void FireBullet(Vector3 direction)
+    {
+        Rigidbody rb = Instantiate(_bulletPrefab, _bulletOrigin.position, Quaternion.identity).GetComponent<Rigidbody>();
+        rb.AddForce(direction * _fireVelocity, ForceMode.Impulse);
+
+    }
+
+    public void ToggleFiring()
+    {
+        _firing = !_firing;
+        
+        if(_firing)
+            StartCoroutine(FiringCycle());
+    }
+}
